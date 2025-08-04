292aa7e098941530bc8c44a66712d075ee7562b0
Ayushverma93 <ayush.04850@gmail.com>
2025-08-04 16:29:02 +0000

    container config done

--- monotonic_arrays.java
+++ monotonic_arrays.java
@@ -1,1 +1,5 @@
 //----------------------------Monotonic arrays------------------------------//
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
