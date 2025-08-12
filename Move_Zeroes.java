35516c5d5685b5a698bcc9fdf6468e3944f6e2ac
Ayushverma93 <ayush.04850@gmail.com>
2025-08-12 16:20:11 +0000

    container config done

--- Move_Zeroes.java
+++ Move_Zeroes.java
@@ -1,3 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int left = 0; // pointer for the position of the next non-zero
+
+         for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                // Swap nums[left] and nums[right]
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+            }
+        }
+    }
+}
