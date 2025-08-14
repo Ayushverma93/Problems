4af1fdb3babe69aa3edcef00a46a7e9a9b71c8ec
Ayushverma93 <ayush.04850@gmail.com>
2025-08-14 17:09:43 +0000

    container config done

--- RenameElement.java
+++ RenameElement.java
@@ -1,3 +1,5 @@
 //-----------------Remove Element --------------------
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
