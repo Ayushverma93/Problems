ac949369d57755ba7558fb7586c9ee1c9356bc1f
Ayushverma93 <ayush.04850@gmail.com>
2025-08-15 10:23:26 +0000

    container config done

--- PowerOfFour.java
+++ PowerOfFour.java
@@ -1,3 +1,7 @@
 //---------------Power Of Four----------------------
 class Solution {
     public boolean isPowerOfFour(int n) {
+        if (n<=0) return false;
+
+        while (n% 4 ==0){
+            
