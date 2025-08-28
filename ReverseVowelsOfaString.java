ad4adbd7ae6a28bc9363ff1654d46fab15d7756d
Ayush Verma <ayush.04850@gmail.com>
2025-08-28 17:56:52 +0000

    Cantainer config dane

--- ReverseVowelsOfaString.java
+++ ReverseVowelsOfaString.java
@@ -1,3 +1,13 @@
 class Solution {
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray(); // convert string to char array.
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        String vowels = "AEIOUaeiou"; // define vowels
+        while (left < right) {
+            // move left pointer until it finds a vowel
+            while (left < right && vowels.indexOf(chars[left]) == -1) {
+                left++;
+            }
