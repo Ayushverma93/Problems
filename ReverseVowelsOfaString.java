1ac02ca6945b56732707253bf0cc1f569e2bd845
Ayush Verma <ayush.04850@gmail.com>
2025-08-28 17:58:08 +0000

    container config done

--- ReverseVowelsOfaString.java
+++ ReverseVowelsOfaString.java
@@ -11,3 +11,19 @@ class Solution {
             while (left < right && vowels.indexOf(chars[left]) == -1) {
                 left++;
             }
+            // move right pointer until it finds a vowel
+            while (left < right && vowels.indexOf(chars[right]) == -1) {
+                right--;
+            }
+
+            // swap the vowels
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(chars); // convert char array back to string
+    }
+}
