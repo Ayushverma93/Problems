87aca6d9f51660e5964f6d8d00d65e41c294214d
Ayush Verma <ayush.04850@gmail.com>
2025-08-30 17:39:54 +0000

    container config done

--- ScoreOfaString.java
+++ ScoreOfaString.java
@@ -1,3 +1,11 @@
 class Solution {
     public int scoreOfString(String s) {
         char[] arr = s.toCharArray();
+
+        int Score = 0;
+        for(int i=1; i< arr.length; i++){
+            Score += Math.abs(arr[i]-arr[i-1]);  // Math.abs is use to give the postive value of this.
+        }
+        return Score;
+    }
+}
