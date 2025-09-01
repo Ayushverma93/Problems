34e96a65851a2451502f4a5b89042d1a60ef766c
Ayush Verma <ayush.04850@gmail.com>
2025-09-01 15:30:13 +0000

    container config done

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -8,3 +8,11 @@ class Solution {
             int minHeight=Math.min(height[left], height[right]);
             int width=right - left;
             int area = minHeight * width;
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
