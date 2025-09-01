7c7d4a6fa6cbe963fa445ccb17a66b5be37cf461
Ayush Verma <ayush.04850@gmail.com>
2025-09-01 15:29:13 +0000

    container config done

--- ContainerWithMostWater.java
+++ ContainerWithMostWater.java
@@ -1,1 +1,7 @@
 //---------COntainer With most Water------------------
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int maxArea=0;
+        
