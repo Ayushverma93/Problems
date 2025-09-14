ff294609e71db538834df5f8079e8587ba626d3d
Ayush Verma <ayush.04850@gmail.com>
2025-09-14 17:23:00 +0000

    container config done

--- RemoveLinkedListElement.java
+++ RemoveLinkedListElement.java
@@ -1,1 +1,5 @@
 //-----------------Remove Linked List Element------------------
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
