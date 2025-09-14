6e04d33584c535c8fae913c275a0aea02f9d2360
Ayush Verma <ayush.04850@gmail.com>
2025-09-14 17:23:28 +0000

    container config done

--- RemoveLinkedListElement.java
+++ RemoveLinkedListElement.java
@@ -3,3 +3,7 @@ class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+        ListNode temp = dummy;
+
+        while (temp.next != null){
+            if (temp.next.val == val){
