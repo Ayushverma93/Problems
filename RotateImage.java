483fd9242e46e0ae6583bbf2561c92d9573e9bb1
Ayush Verma <ayush.04850@gmail.com>
2025-09-17 16:17:11 +0000

    container config done

--- RotateImage.java
+++ RotateImage.java
@@ -27,3 +27,10 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         int[][] rotate = new int[n][m];
+
+        // Rotate elements into new matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                rotate[j][n - i - 1] = matrix[i][j];
+            }
+        }
