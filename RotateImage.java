3bd5b915ae3602988e29eec18b14d7875f925b0e
Ayush Verma <ayush.04850@gmail.com>
2025-09-17 16:19:08 +0000

    container config done

--- RotateImage.java
+++ RotateImage.java
@@ -34,3 +34,13 @@ class Solution {
                 rotate[j][n - i - 1] = matrix[i][j];
             }
         }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                matrix[i][j] = rotate[i][j];
+            }
+        }
+    }
+}
+
+
