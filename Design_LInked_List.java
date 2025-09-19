6ddda2f159111811ed506b06d4973cc2548afa43
Ayush Verma <ayush.04850@gmail.com>
2025-09-19 17:03:29 +0000

    container config done

--- Design_LInked_List.java
+++ Design_LInked_List.java
@@ -30,3 +30,51 @@ class MyLinkedList {
         }
         return temp.data;
     }
+
+    // Add at head
+    public void addAtHead(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    // Add at tail
+    public void addAtTail(int val) {
+        Node newNode = new Node(val);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    // Add at index
+    public void addAtIndex(int index, int val) {
+        if (index < 0 || index > size) return;
+
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        if (index == size) {
+            addAtTail(val);
+            return;
+        }
+
+        Node newNode = new Node(val);
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
+
