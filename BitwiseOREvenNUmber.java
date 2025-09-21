5eb9e9e53d5dafcbd65cd1a1d2d324f7276399b5
Ayush Verma <ayush.04850@gmail.com>
2025-09-21 17:06:12 +0000

    container config done

--- BitwiseOREvenNUmber.java
+++ BitwiseOREvenNUmber.java
@@ -2,4 +2,9 @@
 class Solution {
     public int evenNumberBitwiseORs(int[] nums) {
         int result = 0;
-        
+         for (int num : nums){
+            if(num % 2==0){
+                result |= num;
+            }
+        }
+
