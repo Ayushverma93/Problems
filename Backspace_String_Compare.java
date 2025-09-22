c27d050832104ca2b4ef2247f81b3bfce86d84b9
Ayush Verma <ayush.04850@gmail.com>
2025-09-22 18:43:25 +0000

    container config done

--- Backspace_String_Compare.java
+++ Backspace_String_Compare.java
@@ -3,3 +3,40 @@ class Solution {
         int i = s.length() - 1, j = t.length() - 1;
         int skipS = 0;
         int skipT = 0;
+
+        while (i >= 0 || j >= 0) {
+            // Process backspaces in s
+            while (i >= 0) {
+                if (s.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else break;
+            }
+
+            // Process backspaces in t
+            while (j >= 0) {
+                if (t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else break;
+            }
+
+            // Compare characters
+            if (i >= 0 && j >= 0) {
+                if (s.charAt(i) != t.charAt(j)) return false;
+            } else {
+                if (i >= 0 || j >= 0) return false;
+            }
+
+            i--;
+            j--;
+        }
+        return true;
+    }
+}
