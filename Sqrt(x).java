b7169b9f0dbe17ae3bd01c35eb35171ccd8a4df7
Ayush Verma <ayush.04850@gmail.com>
2025-09-23 18:23:13 +0000

    container config done

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -3,3 +3,21 @@ class Solution {
         if (x == 0 || x == 1) {
             return x;
         }
+        
+        int left = 1;
+        int right = x;
+        int result = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+             if (mid * mid <= x ) {
+                left = mid + 1;
+                result = mid; 
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+}
