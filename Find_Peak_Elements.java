e3e00373212d086d118fd63e357b7bd134478460
Ayush Verma <ayush.04850@gmail.com>
2025-09-24 16:00:00 +0000

    container config done

--- Find_Peak_Elements.java
+++ Find_Peak_Elements.java
@@ -2,3 +2,11 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                // Peak lies on the right side
+                left = mid + 1;
+            }
