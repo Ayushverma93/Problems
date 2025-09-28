02973a05896a66b5001f5e73a13554f99b35c1cd
Ayush Verma <ayush.04850@gmail.com>
2025-09-28 17:24:33 +0000

    container config done

--- Largest_Perimeter_Triangle.java
+++ Largest_Perimeter_Triangle.java
@@ -5,3 +5,14 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
 
+        for(int i = n - 1; i >= 2; i--) {
+            int a = nums[i - 2];
+            int b = nums[i - 1];
+            int c = nums[i];
+            if (a + b > c) {
+                return a + b + c;
+            }
+        }
+        return 0;
+    }
+}
