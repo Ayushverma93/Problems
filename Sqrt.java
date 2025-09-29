ddbf92cb2870365efd02a0b67ad711725e6fd502
Ayush Verma <ayush.04850@gmail.com>
2025-09-29 15:53:18 +0000

    container config done

--- Sqrt.java
+++ Sqrt.java
@@ -10,3 +10,17 @@ class Solution {
         int right = x;
         int result = 0;
         
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Use long to prevent overflow
+            long square = (long) mid * mid;
+            
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                result = mid; // store possible answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
