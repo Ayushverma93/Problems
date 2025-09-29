066fe3e5c0c9a8e8992012aa110a3f331c4cb2c9
Ayush Verma <ayush.04850@gmail.com>
2025-09-29 16:00:22 +0000

    container config done

--- Find_Minimum_in_Rotated_Sorted.java
+++ Find_Minimum_in_Rotated_Sorted.java
@@ -1,1 +1,6 @@
 //---------------------Find Minimum in Rotated Sorted Array----------------------//
+
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length -1 ;
