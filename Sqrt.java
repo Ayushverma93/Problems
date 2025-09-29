e05f73d5983faf3371f164f6dc97c3f75760812b
Ayush Verma <ayush.04850@gmail.com>
2025-09-29 15:52:26 +0000

    container config done

---
+++ Sqrt.java
@@ -0,0 +1,12 @@
+//---------------------Sqrt(x)-------------------------//
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        
+        int left = 1;
+        int right = x;
+        int result = 0;
+        
