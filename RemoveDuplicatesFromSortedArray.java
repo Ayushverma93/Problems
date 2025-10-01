7bf02918ad0d4cf332efed457ba0919362d2f2ca
Ayush Verma <ayush.04850@gmail.com>
2025-10-01 16:03:41 +0000

    container config done

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -6,3 +6,8 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             if(nums[j] != nums[i]){
                 nums[++j]=nums[i];
+                }
+        }
+        return ++j;
+    }
+}
