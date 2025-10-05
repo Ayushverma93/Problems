c0132571d5a2ecc9b77da706ecc21d02d2487c15
Ayush Verma <ayush.04850@gmail.com>
2025-10-05 17:24:34 +0000

    container config done

--- Compute_Alternating_Sum.java
+++ Compute_Alternating_Sum.java
@@ -6,3 +6,10 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (i % 2 == 0)
                 sum += nums[i];
+            else
+                sum -= nums[i]; 
+        }
+
+        return sum;
+    }
+}
