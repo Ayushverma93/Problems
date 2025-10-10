743f656c637db00b745387e0c8673c53c4a1cb91
Ayush Verma <ayush.04850@gmail.com>
2025-10-10 17:37:23 +0000

    container config done

--- Check_if_the_Sentence_is_Pangram.java
+++ Check_if_the_Sentence_is_Pangram.java
@@ -1,2 +1,5 @@
 //----------------------------Check if the Sentence is Pangram----------------------------//
 
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26) return false;
