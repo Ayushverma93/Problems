68a377c5d6f305cf8e30ae041bca9f13b81e7428
Ayush Verma <ayush.04850@gmail.com>
2025-10-11 17:21:46 +0000

    container config done

--- Reverse_Linked_List2.java
+++ Reverse_Linked_List2.java
@@ -9,3 +9,24 @@ class Solution {
         for (int i = 1; i < left; i++) {
             prev = prev.next;
         }
+
+        // Step 3: Reverse the sublist from left to right
+        ListNode curr = prev.next;      // first node of sublist
+        ListNode next = null;
+        ListNode prevSub = null;
+
+        for (int i = 0; i <= right - left; i++) {
+            next = curr.next;
+            curr.next = prevSub;
+            prevSub = curr;
+            curr = next;
+        }
+
+        // Step 4: Connect reversed sublist back
+        prev.next.next = curr;   // tail of reversed part connects to rest
+        prev.next = prevSub;     // connect start of reversed part
+
+        // Step 5: Return new head
+        return dummy.next;
+    }
+}
