7396b61f575d0c155a714bcb82eafabd50178ba7
Ayush Verma <ayush.04850@gmail.com>
2025-10-11 17:20:30 +0000

    container config done

--- Reverse_Linked_List2.java
+++ Reverse_Linked_List2.java
@@ -3,3 +3,9 @@ class Solution {
         // Step 1: Create a dummy node to simplify edge cases
         ListNode dummy = new ListNode(0);
         dummy.next = head;
+
+        // Step 2: Move prev to the node before 'left'
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
