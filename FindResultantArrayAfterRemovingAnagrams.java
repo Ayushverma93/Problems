69696d1363d34cdf86e24ffdd2181220d02f40a1
Ayush Verma <ayush.04850@gmail.com>
2025-10-13 16:18:50 +0000

    container config done

---
+++ FindResultantArrayAfterRemovingAnagrams.java
@@ -0,0 +1,5 @@
+//-----------Find Resultant Array after removing anagrams-------------
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        List<String> result = new ArrayList<>();
+        String prev = "";
