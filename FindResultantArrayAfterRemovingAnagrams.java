864d9c6a29ec1686725f825998ae87e3d740ec4c
Ayush Verma <ayush.04850@gmail.com>
2025-10-13 16:19:26 +0000

    container config done

--- FindResultantArrayAfterRemovingAnagrams.java
+++ FindResultantArrayAfterRemovingAnagrams.java
@@ -3,3 +3,8 @@ class Solution {
     public List<String> removeAnagrams(String[] words) {
         List<String> result = new ArrayList<>();
         String prev = "";
+        for (String word : words){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+
