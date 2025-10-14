ebc6d0f6090e5e9c01f13efcade50c34e100b136
Ayush Verma <ayush.04850@gmail.com>
2025-10-14 16:24:16 +0000

    container config done

--- AdjacentIncreasingSubarray.java
+++ AdjacentIncreasingSubarray.java
@@ -1,1 +1,8 @@
 //---------Adjacent increasing subarray--------------
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n= nums.size();
+        for(int i = 0; i+2*k <=n; i++){
+            boolean first = true, second = true;
+            for(int j = i; j<i+k-1; j++){
+                if(nums.get(j) >= nums.get(j+1)){
