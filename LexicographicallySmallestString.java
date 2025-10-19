1fa96a5206ed17d8d33baa7726302873ea979841
Ayush Verma <ayush.04850@gmail.com>
2025-10-19 08:19:33 +0000

    container config done

--- LexicographicallySmallestString.java
+++ LexicographicallySmallestString.java
@@ -1,1 +1,6 @@
 //-------------lexicographically smaleest string--------------
+class Solution {
+    public String findLexSmallestString(String s, int a, int b) {
+        Set<String> seen = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+        String smallest = s;
