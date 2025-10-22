5400b91a3def2d392b6f059ac8b8676c1698d60c
Ayush Verma <ayush.04850@gmail.com>
2025-10-22 07:41:19 +0000

    container config done

--- MaximumFreqOfAnElementAfterOperationII.java
+++ MaximumFreqOfAnElementAfterOperationII.java
@@ -13,3 +13,11 @@ class Solution {
         events.addAll(count.keySet());
 
         int ans = 0, running = 0;
+        for(long point : events){
+            running += diff.getOrDefault(point, 0);
+            int canTarget= Math.min(running, count.getOrDefault(point, 0) + numOperations);
+            ans = Math.max(ans, canTarget);
+        }
+        return ans;
+    }
+}
