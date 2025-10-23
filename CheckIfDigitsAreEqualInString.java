bcac07abbca3e49d176a71cbf20ee732a76b0c45
Ayush Verma <ayush.04850@gmail.com>
2025-10-23 07:34:57 +0000

    container config done

--- CheckIfDigitsAreEqualInString.java
+++ CheckIfDigitsAreEqualInString.java
@@ -8,3 +8,15 @@ class Solution {
             num1 = (num1 + coefficient * (s.charAt(i) - '0')) % 10;
             num2 = (num2 + coefficient * (s.charAt(i + 1) - '0')) % 10;
         }
+         return num1 == num2;
+    }
+
+    // Returns (n choose k) % 10.
+    private int nCkMod10(int n, int k) {
+        int mod2 = lucas(n, k, 2);
+        int mod5 = lucas(n, k, 5);
+        int[][] lookup = {
+            {0, 6, 2, 8, 4}, // mod2 == 0
+            {5, 1, 7, 3, 9}  // mod2 == 1
+        };
+        return lookup[mod2][mod5];
