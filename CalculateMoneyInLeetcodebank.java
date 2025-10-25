1ed3b89093e83138ba6df4b49d5e4be10cb3377c
Ayush Verma <ayush.04850@gmail.com>
2025-10-25 16:34:35 +0000

    container config done

--- CalculateMoneyInLeetcodebank.java
+++ CalculateMoneyInLeetcodebank.java
@@ -5,4 +5,10 @@ class Solution {
          int days = n % 7;
 
         int total = weeks * 28 + 7 * weeks * (weeks - 1) / 2;
-        
+         for (int i = 1; i<= days; i++){
+            total += weeks +i;
+        }
+        return total;
+
+    }
+}
