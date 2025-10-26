06b6a5711ed3dee0ee4c0ac9e46470c38de6319e
Ayush Verma <ayush.04850@gmail.com>
2025-10-26 11:49:20 +0000

    container config done

--- SimpleBankSystem.java
+++ SimpleBankSystem.java
@@ -6,4 +6,10 @@ class Bank {
         this.balance = balance;
         
     }
+    public boolean transfer(int account1, int account2, long money) {
+        if (!isValidAccount(account1) || !isValidAccount(account2))
+        return false;
+        if(balance[account1 -1] < money)
+        return false;
+
     
