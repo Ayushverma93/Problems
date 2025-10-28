e5b6272bf59f773204150e7ffe556d9fb57928ad
Ayush Verma <ayush.04850@gmail.com>
2025-10-28 13:01:30 +0000

    container config done

--- MakeArrayElementEqualToZero.java
+++ MakeArrayElementEqualToZero.java
@@ -5,3 +5,16 @@ class Solution {
         int total = 0;
         int totalSum = 0;
         for(int v : nums) totalSum += v;
+
+        if(totalSum == 0){
+            int zeros = 0;
+            for (int v: nums) if(v == 0) zeros++;
+            return zeros *2;
+        }
+        long limit = (long) totalSum * (n+1) +n + 10L;
+        for(int i = 0; i<n; i++){
+            if(nums[i] !=0)continue;
+            if(simulate(nums, i, 1, limit)) total++;
+            if(simulate(nums, i, -1, limit)) total++;
+        }
+        return total;
