eb162daf0da9f87e5d6c58ec0b721af5f6d29523
Ayush Verma <ayush.04850@gmail.com>
2025-10-28 13:01:00 +0000

    container config done

--- MakeArrayElementEqualToZero.java
+++ MakeArrayElementEqualToZero.java
@@ -1,1 +1,7 @@
 //-----------make array element equal to zero----------
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        int totalSum = 0;
+        for(int v : nums) totalSum += v;
