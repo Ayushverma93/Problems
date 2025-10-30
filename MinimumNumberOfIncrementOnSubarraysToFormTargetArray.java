4804b6470255b191ca476816b22201e2624a891a
Ayush Verma <ayush.04850@gmail.com>
2025-10-30 14:50:46 +0000

    container config done

--- MinimumNumberOfIncrementOnSubarraysToFormTargetArray.java
+++ MinimumNumberOfIncrementOnSubarraysToFormTargetArray.java
@@ -1,1 +1,4 @@
 //--------------minimum number of increment on subarrays to form a target array-----------------
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int operations = target[0];
