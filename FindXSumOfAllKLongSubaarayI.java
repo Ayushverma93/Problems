e44e41a91a39a0957ae0f9f310a77e43dd2a63fb
Ayush Verma <ayush.04850@gmail.com>
2025-11-04 15:32:42 +0000

    container config done

--- FindXSumOfAllKLongSubaarayI.java
+++ FindXSumOfAllKLongSubaarayI.java
@@ -3,3 +3,12 @@ class Solution {
     public int[] findXSum(int[] nums, int k, int x) {
         int n = nums.length;
         int[] result = new int[n - k + 1];
+        for (int start = 0; start <= n - k; start++) {
+
+            // Frequency array since nums[i] <= 50
+            int[] freq = new int[51];
+
+            // Count frequency of each number in the current window
+            for (int i = start; i < start + k; i++) {
+                freq[nums[i]]++;
+            }
