deac00687a445f2009ff5362daa0f3d41ec013da
Ayush Verma <ayush.04850@gmail.com>
2025-11-05 07:02:32 +0000

    container config done

--- FindXSumOFAllKLongSubArrayII.java
+++ FindXSumOFAllKLongSubArrayII.java
@@ -1,1 +1,17 @@
 //--------find x-sum of all k-long subarrayII---------
+import java.util.*;
+import java.util.function.Consumer;
+
+class Solution {
+
+    static class Node implements Comparable<Node> {
+        int val;
+        int freq;
+        Node(int v, int f) { val = v; freq = f; }
+
+        // Order: higher freq first. If equal freq, higher val first.
+        @Override
+        public int compareTo(Node o) {
+            if (this.freq != o.freq) return Integer.compare(o.freq, this.freq);
+            return Integer.compare(o.val, this.val);
+        }
