93e4fb0de6455a624cd5fdecd26944d7484472d0
Ayush Verma <ayush.04850@gmail.com>
2025-11-12 17:08:06 +0000

    container config done

--- MinimumNUmberOfOperationToMakeAllElementOne.java
+++ MinimumNUmberOfOperationToMakeAllElementOne.java
@@ -1,1 +1,8 @@
 //----------------minimum numbere of operation to make element one -----------------------
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length;
+
+        //count 1
+        int count1 =0;
+        for(int x : nums) if (x == 1) count1++;
