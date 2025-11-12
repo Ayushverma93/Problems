58b1ffb3ed60b693ea633fab04250b2ed4fd892a
Ayush Verma <ayush.04850@gmail.com>
2025-11-12 17:08:38 +0000

    container config done

--- MinimumNUmberOfOperationToMakeAllElementOne.java
+++ MinimumNUmberOfOperationToMakeAllElementOne.java
@@ -6,3 +6,13 @@ class Solution {
         //count 1
         int count1 =0;
         for(int x : nums) if (x == 1) count1++;
+         // if already have one we only need(n - count 1)
+        if(count1 > 0) return n- count1;
+
+        //find shortest subarray with gcd = 1
+        int minLen = Integer.MAX_VALUE;
+
+        for(int i = 0; i<n; i++){
+            int gcd = nums[i];
+            for(int j = 1; j < n; j++){
+                gcd = gcd(gcd, nums[j]);
