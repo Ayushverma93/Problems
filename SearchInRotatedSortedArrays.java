83c2757e6153e7dfb216d64a9bf277f73353c7f8
Ayush Verma <ayush.04850@gmail.com>
2025-12-01 17:00:34 +0000

    Container config done

---
+++ SearchInRotatedSortedArrays.java
@@ -0,0 +1,6 @@
+// ----------------------------Search In Rotated Sorted Array----------------------//
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left, right, mid;
+        int n = nums.length;
