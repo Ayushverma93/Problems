a84326764375825aa5086a62213fa3178baf4d7a
Ayush Verma <ayush.04850@gmail.com>
2026-01-04 15:30:37 +0000

    container config done

--- FourDivisor.java
+++ FourDivisor.java
@@ -1,1 +1,8 @@
 //--------------Four Divisor--------------------
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+
+        for(int num : nums){
+            int count = 0;
+            int sum = 0;
