72eaf67220e5988cb03a996f7091eba306fada34
Ayush Verma <ayush.04850@gmail.com>
2026-01-05 16:05:37 +0000

    container config done

--- MaximumMatrixSum.java
+++ MaximumMatrixSum.java
@@ -1,1 +1,10 @@
 //-------------------Maximum Matrix Sum-----------------------
+class Solution {
+    public long maxMatrixSum(int[][] matrix) {
+        long sumAbs = 0;
+        int negCount = 0;
+        int minAbs = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int val = matrix[i][j];
