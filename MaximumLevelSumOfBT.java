01e6c0044814dd17b53f897d81fe499bdbb620a4
Ayush Verma <ayush.04850@gmail.com>
2026-01-06 17:49:44 +0000

    Container config done

--- MaximumLevelSumOfBT.java
+++ MaximumLevelSumOfBT.java
@@ -4,3 +4,11 @@ class Solution {
         q.offer(root);
         int maxsum = Integer.MIN_VALUE;
         int maxlevel = -1,level = 1;
+
+        while(!q.isEmpty()) {
+
+            int levelsize = q.size();
+            int sum = 0;
+            for(int i = 0; i<levelsize; i++){
+                TreeNode curr = q.remove();
+                sum += curr.val;        
