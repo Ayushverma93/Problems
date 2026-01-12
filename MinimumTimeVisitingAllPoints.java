99b6dc8ccafd3d72ed700093792350a3cdd86492
Ayush Verma <ayush.04850@gmail.com>
2026-01-12 17:10:30 +0000

    container config done

--- MinimumTimeVisitingAllPoints.java
+++ MinimumTimeVisitingAllPoints.java
@@ -1,3 +1,6 @@
 class Solution {
     public int minTimeToVisitAllPoints(int[][] p) {
         int ans = 0;
+        for (int i = 1; i < p.length; i++) {
+            ans += Math.max(
+                Math.abs(p[i][0] - p[i - 1][0]),
