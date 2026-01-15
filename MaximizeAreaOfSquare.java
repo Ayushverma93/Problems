f8a5c8e98a0f5776211d37637018471656fa8277
Ayush Verma <ayush.04850@gmail.com>
2026-01-15 16:17:01 +0000

    container config done

--- MaximizeAreaOfSquare.java
+++ MaximizeAreaOfSquare.java
@@ -1,1 +1,10 @@
 //-------------------Maximize Area of Square----------------------
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+
+        int maxHeight = getMaxGap(hBars);
+        int maxWidth = getMaxGap(vBars);
+
+        int side = Math.min(maxHeight, maxWidth);
+        return side * side;
+    }
