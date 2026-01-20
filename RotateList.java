915fd1f93e4467cf0fea5d22d12f0a2fa87de1f2
Ayush Verma <ayush.04850@gmail.com>
2026-01-20 15:06:37 +0000

    container config done

--- RotateList.java
+++ RotateList.java
@@ -1,1 +1,27 @@
 //----------------Rotate list----------------------
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        // Step 1: Find length and tail
+        ListNode curr = head;
+        int length = 1;
+        while (curr.next != null) {
+            curr = curr.next;
+            length++;
+        }
+
+        // Step 2: Make list circular
+        curr.next = head;
+
+        // Step 3: Reduce k
+        k = k % length;
+        int stepsToNewHead = length - k;
+
+        // Step 4: Find new tail
+        ListNode newTail = curr;
+        while (stepsToNewHead-- > 0) {
+            newTail = newTail.next;
+        }
