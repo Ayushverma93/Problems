f5d5b3ff9a9e7c8d7746e946e3b3eecd877bc771
Ayush Verma <ayush.04850@gmail.com>
2026-01-25 13:14:09 +0000

    container config done

--- MinDiffBetweenHieghestAndLowestKScore.java
+++ MinDiffBetweenHieghestAndLowestKScore.java
@@ -1,1 +1,8 @@
 //----------min diff between highest and lowest k Scores-------------
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if (k == 1) return 0;
+
+        Arrays.sort(nums);
+
+        int minDiff = Integer.MAX_VALUE;
