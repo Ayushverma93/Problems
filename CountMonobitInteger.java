34f64a1547d894218e7c87dc0787ae3484113b7e
Ayush Verma <ayush.04850@gmail.com>
2026-02-01 03:33:56 +0000

    container config done

--- CountMonobitInteger.java
+++ CountMonobitInteger.java
@@ -1,1 +1,16 @@
 //-------------count monobit integer--------------
+class Solution {
+    public int countMonobitIntegers(int n) {
+
+        int count = 1; 
+
+        int val = 1;   
+
+        while (val <= n) {
+            count++;
+            val = (val << 1) | 1; 
+        }
+
+        return count;
+    }
+}
