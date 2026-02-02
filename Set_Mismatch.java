6aa9381768e1381f41fbd63466c1eb075b824d9f
Ayush Verma <ayush.04850@gmail.com>
2026-02-02 16:26:02 +0000

    container config done

--- Set_Mismatch.java
+++ Set_Mismatch.java
@@ -1,3 +1,11 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int dup = -1, missing = -1;
+
+        for(int i = 1; i <= nums.length; i++){
+            int count = 0;
+            for (int j = 0; j < nums.length; j++){
+                if (nums[j] == i){
+                    count++;
+                }
+            }
